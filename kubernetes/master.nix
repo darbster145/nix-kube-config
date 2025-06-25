aea542b04dd11ca0b791b3ff52171de8c6ad6d86
darbster145 <swoopmaster22@proton.me>
2025-06-24 19:41:32 -0600

    k3s disable traefik, servicelb, disable localstorage

--- kubernetes/master.nix
+++ kubernetes/master.nix
@@ -6,6 +6,12 @@
     role = "server";
     # TODO, Use SOPS-NIX here 
     tokenFile = "/var/lib/rancher/k3s/server/token";
+    extraFlags = [
+      "--write-kubeconfig-mode \"0664\""
+      "--disable traefik"
+      "--disable servicelb"
+      "--disable localstorage"
+    ];
     clusterInit = true;
   };
 }
